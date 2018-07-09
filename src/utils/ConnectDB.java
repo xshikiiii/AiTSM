c3e9cbfa135df194cb2d91461ccda4e288ccffa1
ahihi <ahihi@192.168.30.47>
2018-07-09 17:28:02 +0700

    bai goc

---
+++ src/utils/ConnectDB.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ahihi
+ */
+public class ConnectDB {
+    Connection con = null;
+    
+    public ConnectDB() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=AITSM","sa","123456");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnectDB.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+}
