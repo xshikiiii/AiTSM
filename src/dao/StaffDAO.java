3f51a19e55cffbe185e0210941bcf412ca675784
ahihi <ahihi@192.168.30.47>
2018-07-16 15:55:28 +0700

    new update

--- src/dao/StaffDAO.java
+++ src/dao/StaffDAO.java
@@ -5,10 +5,39 @@
  */
 package dao;
 
+import dto.Staff;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+import utils.ConnectDB;
+
+
+
 /**
  *
  * @author ahihi
  */
 public class StaffDAO {
-    
+    List<Staff> lSt = null;
+    Connection con = null;
+
+    public StaffDAO() {
+        lSt = new ArrayList();
+        con = new ConnectDB().getCon();
+    }
+    public Staff create (Staff st){
+        return null;
+    }
+    public List<Staff> readAll(){
+        return lSt;
+    }
+    public List<Staff> readById(String StaffID){
+        return lSt;
+    }
+    public Staff update(Staff st){
+        return null;
+    }
+    public Staff delete(String StaffID){
+        return null;
+    }
 }
--- src/dao/UsersDAO.java
+++ src/dao/UsersDAO.java
@@ -7,8 +7,14 @@ package dao;
 
 import dto.Users;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import utils.ConnectDB;
 
 
@@ -22,7 +28,9 @@ import utils.ConnectDB;
  * @author ahihi
  */
 public class UsersDAO {
-    
+    final private String SQL_USER_CHECK = "SELECT PASS FROM USERS WHERE USERNAME = ?";
+    final private String SQL_USER_CREATE = "INSERT INTO USERS VALUES (?,?,?)";
+    final private String SQL_USER_READALL = "SELECT * FROM USERS";
     List<Users> lU = null;
     Connection con = null;
 
@@ -32,9 +40,34 @@ public class UsersDAO {
     }
     
     public Users create(Users u){
+        try {
+            PreparedStatement pr = con.prepareStatement(SQL_USER_CREATE);
+            pr.setString(1,u.getUsername());
+            pr.setString(2,u.getPassword());
+            pr.setInt(3,u.getRoleid());
+            pr.execute();
+            return u;
+        } catch (SQLException ex) {
+            Logger.getLogger(UsersDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return null;
     }
     public List<Users> readAll(){
+        try {
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(SQL_USER_READALL);
+            if (rs!=null){
+                while(rs.next()){
+                    Users u = new Users();
+                    u.setUsername(rs.getString(1));
+                    u.setPassword(rs.getString(2));
+                    u.setRoleid(rs.getInt(3));
+                    lU.add(u);
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UsersDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return lU;
     }
     public Users searchByUsername(String username){
